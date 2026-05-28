d9b73d57e651d642834d6e1a49d16fc79bfc733a
Sam Olubode <63305676+samolubode@users.noreply.github.com>
2026-05-28 10:50:20 +0200

    Add: number-of-one-bits - submission-1

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    # def hammingWeight(self, n: int) -> int:
+    #     # n & (n - 1) clears out the lowest set bit of the binary
+    #     # once all the lowest set bit is cleared out, the binary becomes zero
+
+    #     count = 0 # counter to keep track of the 1-bits
+
+    #     while n > 0:
+    #         count += 1
+    #         n = n & (n - 1)
+        
+    #     return count
+    
+    def hammingWeight(self, n: int) -> int:
+        count = 0 # counter to keep track of the 1-bits
+
+        while n > 0:
+            count += n & 1 # returns last bit
+            n >>= 1 # shift right by 1: drops last bit
+        
+        return count
+
+        
